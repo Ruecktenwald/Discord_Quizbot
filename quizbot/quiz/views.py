abeadd86923003b35a708796caa14ca166baba32
Peter <ruecktenwald@gmail.com>
2022-08-07 10:10:41 -0400

    fix error

--- quizbot/quiz/views.py
+++ quizbot/quiz/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.views import Response
+from rest_framework.response import Response
 from .models import Question
 from .serializers import RandomQuestionSerializer
 
